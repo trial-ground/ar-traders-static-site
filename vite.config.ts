38a8f6744e556a8c186e81f75a3215e4a635caa3
aboobakker <aboobakker.siddheeq@myminfotech.com>
2025-07-24 15:29:45 +0530

    undo gh

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/ar-traders-static-site/',
+  base: './',
   plugins: [
     react(),
     mode === 'development' &&
